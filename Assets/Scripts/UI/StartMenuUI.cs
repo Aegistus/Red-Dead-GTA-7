3e9764776add144a4322de2e35ecb04a4d3fd437
Aegistus <joe.ballanders@gmail.com>
2023-04-01 18:27:34 -0400

    Add Main Menu Button Functionality

---
+++ Assets/Scripts/UI/StartMenuUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuUI : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game Scene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
