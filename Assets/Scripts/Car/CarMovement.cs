a48652d85d709e79db5ab46ee5dbd49246550654
Aegistus <joe.ballanders@gmail.com>
2023-04-01 13:38:04 -0400

    Add CarMovement Script

---
+++ Assets/Scripts/Car/CarMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class CarMovement : MonoBehaviour
+{
+	public LayerMask groundLayer;
+    public float maxSpeed = 20f;
+	public float turnSpeed = 20f;
+	[Range(0f, 1f)]
+	public float acceleration = .5f;
+	[Range(0f, 1f)]
+	public float brakeDeceleration = .5f;
+	[Range(0f, 1f)]
+	public float coastDeceleration = .1f;
+	float currentSpeed = 0f;
+	Rigidbody rb;
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update()
+	{
+		if (!IsGrounded())
+		{
+			return;
+		}
+		float forwardInput = Input.GetAxis("Vertical");
+		float horizontalInput = Input.GetAxis("Horizontal");
+		if (forwardInput > 0)
+		{
+			currentSpeed = Mathf.Lerp(currentSpeed, maxSpeed, acceleration * Time.deltaTime);
+		}
+		else if (forwardInput < 0)
+		{
+			currentSpeed = Mathf.Lerp(currentSpeed, -maxSpeed, brakeDeceleration * Time.deltaTime);
+		}
+		else
+		{
+			currentSpeed = Mathf.Lerp(currentSpeed, 0f, coastDeceleration * Time.deltaTime);
+		}
+		currentSpeed = Mathf.Clamp(currentSpeed, -maxSpeed, maxSpeed);
+		transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime, Space.Self);
+		transform.Rotate(Vector3.up * horizontalInput * turnSpeed * currentSpeed * Time.deltaTime, Space.Self);
+	}
+
+	public bool IsGrounded()
+	{
+		return Physics.Raycast(transform.position + Vector3.up, Vector3.down, 3f, groundLayer);
+	}
+}
