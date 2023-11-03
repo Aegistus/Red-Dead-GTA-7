ea07fc0efebecf3b95752636b9c9a29dd774918b
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-11-03 17:40:05 -0400

    Add Bandit Movement and Prefab

---
+++ Assets/Scripts/Enemies/BanditMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BanditMovement : MonoBehaviour
+{
+    [SerializeField] float detectionRadius = 30f;
+    [SerializeField] float standDistance = 10f;
+
+    NavMeshAgent navAgent;
+    Transform playerTransform;
+
+    bool hunting = false;
+
+    private void Start()
+    {
+        playerTransform = FindObjectOfType<PlayerHealth>().transform;
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, playerTransform.position);
+        if (distance <= detectionRadius)
+        {
+            if (distance > standDistance)
+            {
+                hunting = true;
+                navAgent.SetDestination(playerTransform.position);
+            }
+            else
+            {
+                navAgent.SetDestination(transform.position);
+            }
+        }
+    }
+}
